f4a14e5a33a435e9dd76cf1a5379a71ebb8a7367
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-10-13 22:27:06 +0800

    API test

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
       />
       {/* <Clock date={new Date()}/> */}
       <Clock />
-
+      <ApiExample />
     </div>
   );
 }
@@ -145,5 +145,34 @@ class Clock extends React.Component{
             )
         }
       }
+
+class ApiExample extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data:''
+    }
+  }
+  callApi=()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then(res=>res.json())
+    .then(json=>{
+      this.setState({
+        data:json.title
+      })
+    })
+  }
+  componentDidMount(){
+    this.callApi();
+  }
+  render(){
+    return(
+      <h3>
+        {this.state.data ? this.state.data : 'Calling data...'}
+      </h3>
+    )
+  }
+}
+
 export default App;
 
